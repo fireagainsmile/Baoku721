691e66301a35c4339cbac17d4dd1e3681efecbe1
guanggg <952577382@qq.com>
2022-11-08 14:35:47 +0800

    add test cases

--- test/BaoKu721.t.sol
+++ test/BaoKu721.t.sol
@@ -2,23 +2,68 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/BaoKu721.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract BaoKuNFTTest is Test {
+    BaokuNFT public baoku;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        string memory name = "baoku";
+        string memory short = "BK";
+        baoku = new BaokuNFT(name, short);
+        baoku.name();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testName() public {
+        assertEq(baoku.name(), "baoku");
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    // normal procedures
+    // 1. issue product
+    // 2. issue follow on product
+    // 3. claim single
+    // 4. claim batch
+    // 5. transfer edition
+    // 6. check owner
+    function testIssueProduct() public{
+        // test issue products
+
+        BaokuNFT.digitalizedProductsMeta memory _dpm;
+        _dpm.name = "name";
+        _dpm.authors = "author";
+        _dpm.productType = "product type";
+        _dpm.productId = "product ID";
+        _dpm.copyRightOwner = "copy right owner";
+        baoku.issueProducts(1, 10, address(111), _dpm, "https://test.url");
+        baoku.issueFollowOnProducts(1, 20);
+        assertEq(baoku.editionTotalSupply(1), 30);
+
+
+        // test claim edition to accounts
+        vm.startPrank(address(111));
+        baoku.claimSingleEdition(1, address(222));
+        address[] memory accounts = new address[](3) ;
+        // [address(333), address(333), address(222)]
+        accounts[0] = address(333);
+        accounts[1] = address(333);
+        accounts[2] = address(222);
+        baoku.claimBatchEdition(1, accounts);
+        assertEq(baoku.current(1),4);
+        assertEq(baoku.editionOwner(1, 1), address(222));
+        assertEq(baoku.editionOwner(1, 2), address(333));
+        assertEq(baoku.editionBalance(address(222), 1), 2);
+        assertEq(baoku.editionBalance(address(222), 1), 2);
+        assertEq(baoku.editionTotalSupply(1), 30);
+        vm.stopPrank();
+
+
+        vm.startPrank(address(222));
+        baoku.transferEdition(1, 1, address (333));
+        assertEq(baoku.editionOwner(1, 1), address(333));
+        assertEq(baoku.editionBalance(address(222), 1), 1);
+        assertEq(baoku.editionBalance(address(333), 1), 3);
+        vm.stopPrank();
+
     }
+
 }
